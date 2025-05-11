e2a8a7d641e0f2cd5189532badc8134d1bd939eb
Carlos Perez <perez@doorstep.com>
2025-05-11 14:38:41 -0600

    Enhance add function to return sum of two numbers

--- jazz1.py
+++ jazz1.py
@@ -1,6 +1,9 @@
-def add():
+def add(a: int, b: int) -> int:
+    """This function adds two number and returns them"""
     print("hello jazzberry")
+    return -1
 
 
 if __name__ == '__main__':
-    add()
+    result = add(1, 2)
+    print(result)
