8247b9d0e9cd04e5125646e4213d5ba35ed51e55
Carlos Perez <perez@doorstep.com>
2025-05-11 20:10:45 -0600

    Remove debug statement and remove incorrect add

--- jazz1.py
+++ jazz1.py
@@ -1,10 +1,9 @@
 def add(a: int, b: int) -> int:
     """This function adds two number and returns them"""
-    print("hello jazzberry")
-    c = 1
-    return a + b + c
+    return a + b
 
 
 if __name__ == '__main__':
+    print("hello jazzberry")
     result = add(1, 2)
     print(result)
