35df3512ce63b4cdc6cdb38c7aeb3bf55e85ca0b
Carlos Perez <perez@doorstep.com>
2025-05-24 09:13:37 -0600

    Hello world (#1)
    * Hello world
    
    * Tidy with adding main check
    
    * Enhance add function to return sum of two numbers
    
    * Improve the add function
    
    * Remove debug statement and remove incorrect add
    
    * Add FizzBuzz game
    
    * GitHub seems stuck
    
    * Trigger jazzberry?
    
    * Trigger jazzberry
    
    * Trigger jazzberry

---
+++ jazz1.py
@@ -0,0 +1,9 @@
+def add(a: int, b: int) -> int:
+    """This function adds two number and returns them"""
+    return a + b
+
+
+if __name__ == '__main__':
+    print("hello jazzberry")
+    result = add(1, 2)
+    print(result)
